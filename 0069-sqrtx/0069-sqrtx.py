67a96cefbb29a29709b9fa9ed81f196152a72272
pestopasta74 <144602183+pestopasta74@users.noreply.github.com>
2024-02-01 20:51:16 +0000

    Time: 34 ms (89.77%), Space: 16.5 MB (62.38%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,18 @@
+class Solution:
+    def __init__(self):
+        self.loop = 0
+    
+    def mySqrt(self, x: int) -> int:
+        if x <= 0:
+            return 0
+        constant_x = x
+
+        def sqrt_loop(x, constant_x):
+            x = (x + constant_x / x) / 2 
+            self.loop += 1
+            if self.loop <= 20:
+                return sqrt_loop(x, constant_x)
+            else:
+                return floor(x)
+
+        return sqrt_loop(x, constant_x)
